b4ad3ab238fbe78d678cb0afb7a0456ab8d790bb
Alas1132 <67383085+Alas1132@users.noreply.github.com>
2020-06-24 22:27:11 +0600

    added all file for turn

---
+++ in.js
@@ -0,0 +1,16 @@
+$('.count').counterUp({
+
+   delay:10,
+   time:1000
+
+
+});
+      
+
+  function topFunction(){
+
+      document.body.scrollTop=0;
+      document.documentElement.scrollTop=0;
+
+
+  }
